57617dd8365692c51c5b5e6b359b4865ad85fae9
Muhammad Azhar Iqbal <muhammad1azhar2@gmail.com>
2020-08-25 23:27:34 +0500

    8/25/2020

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function showDiv() {
+    document.getElementById('started-option').style.display = "none";
+    document.getElementById('amount-dropdown').style.display = "block";
+ }
+ function showSecDiv() {
+    document.getElementById('amount-dropdown').style.display = "none";
+    document.getElementById('data').style.display = "block";
+ }
+ function showLastDiv() {
+    document.getElementById('data').style.display = "none";
+    document.getElementById('on_sub').style.display = "block";
+ }
